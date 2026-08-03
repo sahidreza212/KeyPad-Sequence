60c0994e581f6c019ead81cb3262b318250a68aa
Sahid Reza <sahidreza976@gmail.com>
2026-08-03 09:12:36 +0530

    Add this

--- src/main/java/org/example/PrintSequence.java
+++ src/main/java/org/example/PrintSequence.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class PrintSequence {
     public static String printSequence(String str[] ,String input){
-
+         String output = "";
+         int n = input.length();
     }
     public static void main(String[] args) {
 
