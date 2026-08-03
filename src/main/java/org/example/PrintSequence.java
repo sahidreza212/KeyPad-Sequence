2d45e1f4d451db99fde86d6502e8b4610274ee93
Sahid Reza <sahidreza976@gmail.com>
2026-08-03 09:11:00 +0530

    Add printSequence function

--- src/main/java/org/example/PrintSequence.java
+++ src/main/java/org/example/PrintSequence.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class PrintSequence {
+    public static String printSequence(String str[] ,String input){
+
+    }
     public static void main(String[] args) {
 
     }
