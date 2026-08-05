f75a99913e5cde6f310e2faf8450668d0f94f70a
Sahid Reza <sahidreza976@gmail.com>
2026-08-05 06:51:03 +0530

    Add this Print all duplicate

--- src/main/java/org/example/PrintSequence.java
+++ src/main/java/org/example/PrintSequence.java
@@ -4,8 +4,26 @@ public class PrintSequence {
     public static String printSequence(String str[] ,String input){
          String output = "";
          int n = input.length();
+         for(int i = 0;i<n;i++){
+
+             char ch = Character.toUpperCase(input.charAt(i));
+             if(ch == ' '){
+                 output = output +'0';
+             }else {
+                  int position = ch - 'A';
+                  output = output+str[position];
+             }
+         }
+         return output;
     }
     public static void main(String[] args) {
-
+        String str[] = {
+                "2","22","222","3","33","333",
+                "4","44","444","5","55","555",
+                "6","66","666","7","77","777","7777",
+                "8","88","888","9","99","999","9999"
+        };
+        String input = "mamun HassaN";
+        System.out.println(printSequence(str,input));
     }
 }
---
+++ src/main/java/org/example/Print_Duplicate.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class Print_Duplicate {
+}
