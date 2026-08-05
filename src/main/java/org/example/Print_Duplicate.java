2b195627d5b32310e65b31b34bf739699d6cffe8
Sahid Reza <sahidreza976@gmail.com>
2026-08-05 06:54:58 +0530

    Add this

--- src/main/java/org/example/Print_Duplicate.java
+++ src/main/java/org/example/Print_Duplicate.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Print_Duplicate {
     public static void printDuplicate(String str){
 
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
     }
     public static void main(String[] args) {
 
