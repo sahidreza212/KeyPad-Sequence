e2b21d92b67cf384711e0cbc043bbd5642e59275
Sahid Reza <sahidreza976@gmail.com>
2026-08-05 06:58:01 +0530

    add this

--- src/main/java/org/example/Print_Duplicate.java
+++ src/main/java/org/example/Print_Duplicate.java
@@ -11,8 +11,14 @@ public class Print_Duplicate {
         for(int i = 1;i< arr.length;i++){
             if(arr[i] == arr[i-1]){
                 count++;
+            }else {
+                if(count > 1){
+                    System.out.println(arr[i-1] +"-->"+count);
+                }
+                count = 1;
             }
         }
+
     }
     public static void main(String[] args) {
 
