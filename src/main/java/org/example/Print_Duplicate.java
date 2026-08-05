5494736127f39f2aea0baa4072454d04b19117c1
Sahid Reza <sahidreza976@gmail.com>
2026-08-05 06:53:09 +0530

    Add print duplicate function

--- src/main/java/org/example/Print_Duplicate.java
+++ src/main/java/org/example/Print_Duplicate.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Print_Duplicate {
+    public static void printDuplicate(String str){
+
+    }
     public static void main(String[] args) {
 
+        String str = "Sahid Reza";
     }
 }
